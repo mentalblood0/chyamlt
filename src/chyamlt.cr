ca964d1b70fa09959adf792d705a04542f97817b
mentalblood0 <necheporenko.stepan@yandex.ru>
2025-03-26 18:10:10 +0300

    move from websocket to http

--- src/chyamlt.cr
+++ src/chyamlt.cr
@@ -1,6 +1,7 @@
 require "yaml"
+require "http"
 require "http/server"
-require "http/web_socket"
+require "http/server/handler"
 require "uri"
 
 module Chyamlt
@@ -50,43 +51,51 @@ module Chyamlt
   {% end %}
 
   class Server
+    class_property messages_path
+    class_property messages_file
     @@messages_path : Path = Chyamlt.dir / "server.yml"
     @@messages_file = File.new @@messages_path, "a"
 
-    @messages = [] of ServerMessage
+    class MessageHandler
+      include HTTP::Handler
 
-    def initialize(@host : String, @port : Int32)
-      Dir.mkdir_p @@messages_path.parent
-      File.open @@messages_path do |file|
-        @messages = Array(ServerMessage).from_yaml file rescue [] of ServerMessage
-      end
+      @messages = [] of ServerMessage
 
-      @handler = HTTP::WebSocketHandler.new do |ws, ctx|
-        address = ctx.request.remote_address.to_s
-        Log.info { "SERVER : Connection from client #{address}" }
-        ws.on_message do |text|
-          Log.debug { "SERVER : Parsing new messages" }
-          pkg = begin
-            ClientPackage.from_yaml text
-          rescue ex
-            Log.error { "CLIENT : Error parsing ClientPackage : #{ex.message}" }
-            next
-          end
-          Log.info { "SERVER : #{pkg.messages.size} messages from client #{address} (#{pkg.saved} are already saved)" }
-
-          new_messages = pkg.messages
-            .select { |client_message| client_message.text.size > 0 }
-            .map { |client_message| ServerMessage.new address, client_message }
-
-          @@messages_file.print new_messages.to_yaml[4..]
-          @@messages_file.flush
-          @messages += new_messages
-
-          ws.send ServerPackage.new(@messages[pkg.saved..]).to_yaml
+      def initialize
+        Dir.mkdir_p Server.messages_path.parent
+        File.open Server.messages_path do |file|
+          @messages = Array(ServerMessage).from_yaml file rescue [] of ServerMessage
         end
       end
 
-      @server = HTTP::Server.new [@handler]
+      def call(context)
+        Log.debug { "SERVER : MessageHandler : Parsing new messages" }
+        text = IO::Sized.new(context.request.body.not_nil!, 16 * 1024).gets_to_end
+        address = context.request.remote_address.to_s
+        pkg = begin
+          ClientPackage.from_yaml text
+        rescue ex
+          Log.error { "SERVER : MessageHandler : Error parsing ClientPackage : #{ex.message}" }
+          return
+        end
+
+        new_messages = pkg.messages
+          .select { |client_message| client_message.text.size > 0 }
+          .map { |client_message| ServerMessage.new address, client_message }
+        Log.info { "SERVER : MessageHandler : #{new_messages.size} new messages from client #{address} (#{pkg.saved} are already saved)" }
+
+        if new_messages.size > 0
+          Server.messages_file.print new_messages.to_yaml[4..]
+          Server.messages_file.flush
+          @messages += new_messages
+        end
+
+        context.response.print ServerPackage.new(@messages[pkg.saved..]).to_yaml
+      end
+    end
+
+    def initialize(@host : String, @port : Int32)
+      @server = HTTP::Server.new [MessageHandler.new]
       @address = @server.bind_tcp @host, @port
       spawn @server.listen
     end
@@ -109,30 +118,30 @@ module Chyamlt
     @size = 0
 
     def initialize(@host : String, @port : Int32)
+      @address = "http://#{@host}:#{@port}"
+      @client = HTTP::Client.new URI.parse @address
+
       Dir.mkdir_p @@messages_path.parent
       Dir.mkdir_p @@input_path.parent
       File.each_line @@messages_path do |line|
         @size += 1 if line.starts_with? "- "
       end
+    end
 
-      @address = URI.parse "wb://#{@host}:#{@port}"
-      @socket = HTTP::WebSocket.new @address
-      @socket.on_message do |text|
-        Log.debug { "CLIENT : Parsing received messages" }
-        pkg = begin
-          ServerPackage.from_yaml text
-        rescue ex
-          Log.error { "CLIENT : Error parsing ServerPackage : #{ex.message}" }
-          next
-        end
-        Log.info { "CLIENT : #{pkg.messages.size} messages from server #{@address}" }
-
-        @size += pkg.messages.size
-        @@messages_file.print pkg.messages.to_yaml[4..]
-        @@messages_file.flush
-        File.delete @@input_path
+    def add_messages(response_body : String)
+      Log.debug { "CLIENT : Parsing received messages" }
+      pkg = begin
+        ServerPackage.from_yaml response_body
+      rescue ex
+        Log.error { "CLIENT : Error parsing ServerPackage : #{ex.message}" }
+        return
       end
-      spawn @socket.run
+      Log.info { "CLIENT : #{pkg.messages.size} messages from server #{@address}" }
+
+      @size += pkg.messages.size
+      @@messages_file.print pkg.messages.to_yaml[4..]
+      @@messages_file.flush
+      File.delete @@input_path
     end
 
     def monitor
@@ -149,8 +158,13 @@ module Chyamlt
             next
           end
           Log.debug { "CLIENT : Sending new messages" }
-          @socket.send ClientPackage.new(@size, new_messages).to_yaml
+          response = @client.post "/", body: ClientPackage.new(@size, new_messages).to_yaml
           Log.debug { "CLIENT : Sent new messages" }
+          if !response.success?
+            Log.error { "CLIENT : Non-success response #{response.status}" }
+          else
+            add_messages response.body
+          end
         end
         last_check = Time.utc
         sleep 0.2.seconds
@@ -168,6 +182,8 @@ module Chyamlt
     end
   end
 end
+
+Log.setup level: Log::Severity::Debug
 
 server = Chyamlt::Server.new "localhost", 3000
 client = Chyamlt::Client.new "localhost", 3000
